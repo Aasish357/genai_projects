c7f42c77c0061632aa41be54dbb69461d26defcb
Aasish bharadwaja <143341468+Aasish357@users.noreply.github.com>
2025-08-25 11:27:17 +0530

    Add files via upload

---
+++ aasish.py
@@ -0,0 +1,4 @@
+x = "hellow my name is aasish bharadwaja i am learning python and i am enjoying it for now and then i will be a good programmer and i will be a good human being    "
+y= x.split()
+longest_word=max(y,key=len)
+print (longest_word)
